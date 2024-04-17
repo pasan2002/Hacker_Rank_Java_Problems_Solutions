e840befe2ba6a21108c663ac87252a27c3036817
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-04-17 21:23:57 +0530

    first day

---
+++ Java_Loops_I/solution.java
@@ -0,0 +1,24 @@
+package Java_Loops_I;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println(N + " " + "x " + i + " = " + (N * i));
+        }
+
+        bufferedReader.close();
+    }
+}
+
