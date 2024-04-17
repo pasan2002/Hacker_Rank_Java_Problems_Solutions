ae27a1246f1f846240a4e158a8da64950bde41a7
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-04-17 19:45:54 +0530

    first day

---
+++ Java_Stdin_and_Stdout_I/solution.java
@@ -0,0 +1,17 @@
+package Java_Stdin_and_Stdout_I;
+
+import java.util.Scanner;
+
+public class solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ Welcome_to_Java/solution.java
@@ -0,0 +1,9 @@
+package Welcome_to_Java;
+
+public class solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
